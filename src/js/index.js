1a0ed7f1bb75324c2f64aaa46fa69f6b31e2dc57
YuliaKur <yulkur.reignsupreme@gmail.com>
2024-02-25 12:28:24 +0200

    swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
-'use strict';
+import tagline from './modules/tagline.js';
+tagline();
+
+import swiper from './modules/swiper.js';
+swiper();
+
+// import timer from './modules/timer.js';
+// timer();
+
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
-
-
-import tagline from "./modules/tagline";
-tagline();
