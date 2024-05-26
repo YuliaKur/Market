4c87511780fcefecc62e4c6f05b7d67f6e6cd1f8
YuliaKur <yulkur.reignsupreme@gmail.com>
2024-05-26 23:23:41 +0300

    sec promotion

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,8 @@ tagline();
 import swiper from './modules/swiper.js';
 swiper();
 
-// import timer from './modules/timer.js';
-// timer();
+import timer from './modules/timer.js';
+timer();
 
 // import mobileNav from './modules/mobile-nav.js';
 // mobileNav();
